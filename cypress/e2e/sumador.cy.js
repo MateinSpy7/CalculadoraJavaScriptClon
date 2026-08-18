8e747af4a4167efc4e0a352227283eee59eb8857
Mateo Quintanilla <quintanillacalvimateo@gmail.com>
2026-08-18 18:54:19 -0400

    Multiplicacion agregada

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -7,3 +7,13 @@ describe("Sumador", () => {
     cy.get("#resultado-div").should("contain", "9");
   });
 });
+
+describe("Multiplicador", () => {
+  it("Shows the product to the user", () => {
+    cy.visit("/");
+    cy.get("#multiplicar-form #primer-numero").type(4);
+    cy.get("#multiplicar-form #segundo-numero").type(5);
+    cy.get("#multiplicar-button").click();
+    cy.get("#multiplicar-resultado-div").should("contain", "20");
+  });
+});
--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const multiplicarForm = document.querySelector("#multiplicar-form");
+const multiplicarDiv = document.querySelector("#multiplicar-resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault(); //pra no recargar la pagina
@@ -12,4 +15,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  multiplicarDiv.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
